fba588bd5588efda4a21856eda372f4cc3054cdb
vani33 <kothapallivanisree292@gmail.com>
2021-11-14 14:49:33 +0530

    Jenkins Example first commit

---
+++ src/main/java/com/example/jenkinsExample/JenkinsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkinsExample/JenkinsExampleApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.jenkinsExample;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsExampleApplicationTests {
+
+	static Logger logger = LoggerFactory.getLogger(JenkinsExampleApplicationTests.class);
+	
+	@Test
+	void contextLoads() {
+		logger.info("***In a JenkinsExampleApplicationTests****");
+	}
+
+}
